566866940ae8ffbbb88a8f5f34f79e3b8bf92ac5
Nikhitha Reddy <katukurinikhita@gmail.com>
2017-08-26 17:53:22 -0400

    Modified apache.pp

--- apache.pp
+++ apache.pp
@@ -1,7 +1,33 @@
-package { 'httpd':
+#variable decleration
+$package_name = "httpd"  
+
+#array
+# $package_name = ["httpd", "apache", "Welcome to Puppet"] 
+
+#hashes
+# $package_name= {
+    
+#     'redhat' => 'httpd',
+#     'ubuntu' => 'apache',
+#     'message' => 'Welcome to puppet'
+# }
+
+# $package_name = 'test'
+# # $package_string = "This is puppet ${package_name}"
+
+#Heredoc
+# $package_multiple = @("START")
+# This is puppet example
+# today is Sat ${package_name}
+# START
+# #This is dummy line
+
+# notify { $package_multiple: } 
+
+package { $package_name:
     ensure => installed,    
 }
-service { "httpd":
+service { $package_name:
     ensure     => running,
     enable     => true,
 }
