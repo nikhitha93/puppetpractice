3d3966030375b7afd5cc04aea91289ea230a6bb4
Nikhitha Reddy <katukurinikhita@gmail.com>
2017-08-26 18:11:35 -0400

    Added facter.pp

---
+++ facter.pp
@@ -0,0 +1,19 @@
+if $facts['os']['family'] = 'RedHat' {
+    $package_name = "httpd"
+}
+
+elsif $facts['os']['family'] = 'Debian' {
+    $package_name = "apache"
+}
+
+package { $package_name:
+    ensure => installed,
+    
+}
+
+service { $package_name:
+    ensure     => running,
+    enable     => true,
+
+}
+
